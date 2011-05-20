4dad87ebc6026de6be8bf50230e1be12ee2223c1
Marek Kubica <marek@xivilization.net>
2011-05-20 12:52:54 +0200

    Changed Send & Receive into Sendreceive

--- 2.Shift/shift.c
+++ 2.Shift/shift.c
@@ -53,24 +53,14 @@ main(int argc, char *argv[])
 
 
     for (i = 0; i < shifts; i++) {
-	if (myid == 0) {
-	    MPI_Send(&values[0], 1, MPI_INT, lnbr, 10, MPI_COMM_WORLD);
-	    for (j = 1; j < 100 / np; j++) {
-		values[j - 1] = values[j];
-	    }
-	    MPI_Recv(&values[100 / np - 1], 1, MPI_INT, rnbr, 10,
-		     MPI_COMM_WORLD, &status);
-	} else {
-	    int buf = values[0];
-	    for (j = 1; j < 100 / np; j++) {
-		values[j - 1] = values[j];
-	    }
-	    MPI_Recv(&values[100 / np - 1], 1, MPI_INT, rnbr, 10,
-		     MPI_COMM_WORLD, &status);
-	    MPI_Send(&buf, 1, MPI_INT, lnbr, 10, MPI_COMM_WORLD);
-	}
-    }
+        MPI_Sendrecv(&values[0], 1, MPI_INT, lnbr, 10,
+                &values[100 / np -1], 1, MPI_INT, rnbr, 10,
+                MPI_COMM_WORLD, &status);
 
+        for (j = 1; j < 100 / np; j++) {
+            values[j - 1] = values[j];
+        }
+    }
 
     if (myid == 0) {
 	int flag = 1;
