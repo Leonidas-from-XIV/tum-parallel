827014979308c30d530bedddff77df627bd7d5d1
Marek Kubica <marek@xivilization.net>
2011-05-25 12:48:53 +0200

    K&R style

--- 3.PI/pi.c
+++ 3.PI/pi.c
@@ -7,21 +7,20 @@
 
 const int n = 10000000;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  int i;
-  double w,x,sum,pi;
+    int i;
+    double w, x, sum, pi;
 
 /* calculate the interval size */
-  w=1.0/n;
-  sum=0.0;
-  for (i=1;i<=n;i++)
-  {
-    x=w*((double)i-0.5);
-    sum=sum+f(x);
-  }
-  pi=w*sum;
+    w = 1.0 / n;
+    sum = 0.0;
+    for (i = 1; i <= n; i++) {
+	x = w * ((double) i - 0.5);
+	sum = sum + f(x);
+    }
+    pi = w * sum;
 
-  printf( "computed pi = %24.16g\n", pi );
-  return 0;
+    printf("computed pi = %24.16g\n", pi);
+    return 0;
 }
