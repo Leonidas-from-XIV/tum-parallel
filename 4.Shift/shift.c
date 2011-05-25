e5f280f1e9d3b585043314c8caa55cd219087d91
Marek Kubica <marek@xivilization.net>
2011-05-25 17:03:19 +0200

    Added an Isend call

--- 4.Shift/shift.c
+++ 4.Shift/shift.c
@@ -44,15 +44,20 @@ main(int argc, char *argv[])
 	values[0] = 1;
     }
 
+    MPI_Request req;
 
     for (i = 0; i < shifts; i++) {
 	if (myid == 0) {
-	    MPI_Send(&values[0], 1, MPI_INT, lnbr, 10, MPI_COMM_WORLD);
+            int buf = values[0];
+	    MPI_Isend(&buf, 1, MPI_INT, lnbr, 10, MPI_COMM_WORLD, &req);
+
 	    for (j = 1; j < 100 / np; j++) {
 		values[j - 1] = values[j];
 	    }
+
 	    MPI_Recv(&values[100 / np - 1], 1, MPI_INT, rnbr, 10,
 		     MPI_COMM_WORLD, &status);
+            MPI_Wait(&req, &status);
 	} else {
 	    int buf = values[0];
 	    for (j = 1; j < 100 / np; j++) {
