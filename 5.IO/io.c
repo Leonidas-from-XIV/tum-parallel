4c714cf2f2b69927744adfe44bdb2d597e44a803
Marek Kubica <marek@xivilization.net>
2011-05-31 11:19:58 +0200

    Added first version of IO program that writes to files

--- 5.IO/io.c
+++ 5.IO/io.c
@@ -3,17 +3,25 @@
 
 int main(int argc, char** argv)
 {
-    int rank, size;
+    int rank, size, normalized;
+    FILE* f;
+    char filename[255];
+
     if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
         printf("Error in init\n");
         return 1;
     }
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    normalized = rank + 1;
 
-    printf("Hello from process %d of %d\n",
-            rank+1, size);
+    sprintf(filename, "rank%d.file", normalized);
 
+    f = fopen(filename, "w");
+    fprintf(f, "Hello from process %d of %d\n",
+            normalized, size);
+
+    fclose(f);
     MPI_Finalize();
     return 0;
 }
