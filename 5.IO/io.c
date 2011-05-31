674ba8f94320df5913c5ffe6a304cafb806d028c
Marek Kubica <marek@xivilization.net>
2011-05-31 12:05:10 +0200

    Made processes write in a synchronized manner into one file

--- 5.IO/io.c
+++ 5.IO/io.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <mpi.h>
 
 int main(int argc, char** argv)
 {
     int rank, size, normalized;
-    FILE* f;
-    char filename[255];
+    char buf[255];
+    MPI_File f;
+    MPI_Status status;
 
     if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
         printf("Error in init\n");
@@ -15,13 +17,13 @@ int main(int argc, char** argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     normalized = rank + 1;
 
-    sprintf(filename, "rank%d.file", normalized);
+    // format the message to write
+    sprintf(buf, "Hello from process %d of %d\n", normalized, size);
 
-    f = fopen(filename, "w");
-    fprintf(f, "Hello from process %d of %d\n",
-            normalized, size);
+    MPI_File_open(MPI_COMM_WORLD, "rank1.file", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f);
+    MPI_File_write_ordered(f, buf, strlen(buf), MPI_CHAR, &status);
 
-    fclose(f);
+    MPI_File_close(&f);
     MPI_Finalize();
     return 0;
 }
