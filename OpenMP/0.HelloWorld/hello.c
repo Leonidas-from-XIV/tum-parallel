46a3c582bcd6a4ce735da236b3bce273b65433e3
Marek Kubica <marek@xivilization.net>
2011-06-15 12:54:50 +0200

    Made the OpenMP Hello World example run.
    Set OMP_NUM_THREADS to get interesting results.

--- OpenMP/0.HelloWorld/hello.c
+++ OpenMP/0.HelloWorld/hello.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <omp.h>
 
 int main(int argc, char **argv)
 {
-
-    printf("Hello World\n");
+#pragma omp parallel
+    {
+    printf("Hello World from %d of %d.\n",
+            omp_get_thread_num() + 1,
+            omp_get_num_threads());
+    }
 
     return 0;
 }
