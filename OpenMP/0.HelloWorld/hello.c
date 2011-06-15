4ed7ed9f44142d052cd1436b5a31ed9b4b41b40f
Marek Kubica <marek@xivilization.net>
2011-06-15 12:41:14 +0200

    K&R & Unix line endings

--- OpenMP/0.HelloWorld/hello.c
+++ OpenMP/0.HelloWorld/hello.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 
- printf( "Hello World\n");
+    printf("Hello World\n");
 
- return 0;
+    return 0;
 }
