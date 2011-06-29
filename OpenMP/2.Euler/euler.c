20a7cbddde6e7640ec09a13bae25ab39e2c55f9f
Marek Kubica <marek@xivilization.net>
2011-06-29 12:59:59 +0200

    K&R-ize euler code

--- OpenMP/2.Euler/euler.c
+++ OpenMP/2.Euler/euler.c
@@ -2,39 +2,36 @@
 #define M 4000000
 void main()
 {
-  double uc[M+1], un[M+1];
-  double r, r1, dx, dt;
-  int timcnt;
-  int i,j;
+    double uc[M + 1], un[M + 1];
+    double r, r1, dx, dt;
+    int timcnt;
+    int i, j;
 
-  dx = 0.1e-2;
-  dt = 0.1e-6;
-  timcnt = 250;
+    dx = 0.1e-2;
+    dt = 0.1e-6;
+    timcnt = 250;
 
-  r = dt/(dx*dx);
-  r1 = 1-2*r;
+    r = dt / (dx * dx);
+    r1 = 1 - 2 * r;
 
-    for( i=0; i<M; i++ ) {
-      uc[i] = 1.0;
+    for (i = 0; i < M; i++) {
+	uc[i] = 1.0;
     }
- 
+
     uc[0] = 0.0;
     uc[M] = 0.0;
-    for( j=0; j<timcnt; j++ ) {
-      for( i=1; i<M; i++ ) {
-        un[i] = r*(uc[i-1]+uc[i+1])+r1*uc[i];
-      }
-      for( i=1; i<M; i++ ) {
-        uc[i] = un[i];
-      }
+    for (j = 0; j < timcnt; j++) {
+	for (i = 1; i < M; i++) {
+	    un[i] = r * (uc[i - 1] + uc[i + 1]) + r1 * uc[i];
+	}
+	for (i = 1; i < M; i++) {
+	    uc[i] = un[i];
+	}
     }
 
-  double sum=0.0;
-  for( i=1; i<M-1; i++ ) {
-  	sum=sum+uc[i];
-  }
-  printf( "sum  %9.8f\n", sum );
+    double sum = 0.0;
+    for (i = 1; i < M - 1; i++) {
+	sum = sum + uc[i];
+    }
+    printf("sum  %9.8f\n", sum);
 }
-
-
-
