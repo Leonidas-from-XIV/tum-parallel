2c8b3174e0c71cd5412153c799d7aca29a32792a
Marek Kubica <marek@xivilization.net>
2011-07-06 13:27:09 +0200

    Implemented some OpenMP primitives and time calculation

--- OpenMP/3.LoadBalancing/loadbalancing.c
+++ OpenMP/3.LoadBalancing/loadbalancing.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 
 #define N_R      79
 #define N_I      100
@@ -19,16 +20,33 @@ void main()
     char schedenv[13] = "OMP_SCHEDULE";
     char *sched;
     int i, j;
+    double* time_per_thread;
+    int num_threads;
 
     sched = getenv(schedenv);
     v_r_step = (V_R_MAX - V_R_MIN) / N_R;
     v_i_step = (V_I_MAX - V_I_MIN) / N_I;
 
+    char *num_threads_env = getenv("OMP_NUM_THREADS");
+    if (!num_threads_env) {
+        num_threads = 1;
+    }
+    else {
+        num_threads = atoi(num_threads_env);
+    }
+
+    time_per_thread = calloc(num_threads, sizeof(double));
+
     for (i = 1; i <= N_R; i++) {
 	v_r = V_R_MIN + (i - 1) * v_r_step;
+#pragma omp parallel for
 	for (j = 1; j <= N_I; j++) {
 	    v_i = V_I_MIN + (j - 1) * v_i_step;
+            double start = omp_get_wtime();
 	    field[i - 1][j - 1] = icalman(v_r, v_i);
+            double end = omp_get_wtime();
+            int current_thread = omp_get_thread_num();
+            time_per_thread[current_thread] += end - start;
 	}
     }
 
@@ -48,6 +66,10 @@ void main()
 	printf("\n");
     }
     printf("%s=%s\n", schedenv, sched);
+
+    for (i = 0; i < num_threads; i++) {
+        printf("Execution time of thread %d: %f\n", i+1, time_per_thread[i]);
+    }
 }
 
 #define CMAX 10000
